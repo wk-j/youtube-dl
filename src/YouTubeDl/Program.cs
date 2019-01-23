317458bccf4b1c8c79c994c49ebe22d9a79a7a99
wk-j <somnuk.wk@gmail.com>
2019-01-23 16:35:12 +0700

    Initial commit

---
+++ src/YouTubeDl/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NYoutubeDL;
+
+namespace YouTubeDl {
+    class Program {
+        static void Main(string[] args) {
+            var dl = new YoutubeDL();
+            dl.Options.FilesystemOptions.Output = ".";
+            dl.Options.PostProcessingOptions.ExtractAudio = true;
+            dl.VideoUrl = args[0];
+
+            dl.StandardErrorEvent += (s, o) => {
+                Console.WriteLine("?? {0}", o);
+            };
+
+            dl.StandardOutputEvent += (s, o) => {
+                Console.WriteLine(":: {0}", o);
+            };
+
+            dl.Download();
+        }
+    }
+}
